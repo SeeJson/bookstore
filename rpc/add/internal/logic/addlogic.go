6b02284ab0a58d25635f43b705125a3a0edf31c8
xiemingliu <xmldn123@outlook.com>
2023-04-22 17:46:28 +0800

    test go-zero

---
+++ rpc/add/internal/logic/addlogic.go
@@ -0,0 +1,39 @@
+package logic
+
+import (
+	"bookstore/rpc/model"
+	"context"
+
+	"bookstore/rpc/add/internal/svc"
+	"bookstore/rpc/add/pb"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type AddLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewAddLogic(ctx context.Context, svcCtx *svc.ServiceContext) *AddLogic {
+	return &AddLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *AddLogic) Add(in *__.AddReq) (*__.AddResp, error) {
+	// todo: add your logic here and delete this line
+	_, err := l.svcCtx.Modele.Insert(l.ctx, &model.Book{
+		Book:  in.Book,
+		Price: in.Price,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &__.AddResp{
+		Ok: true,
+	}, nil
+}
